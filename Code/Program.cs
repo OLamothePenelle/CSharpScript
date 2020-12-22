0f4ff3ac56d432b1bb89e60079d24bcecf17c5c5
olamothepenelle <olamothepenelle@protonmail.com>
2020-12-21 23:02:22 -0500

    Add some functionality to template, add verbose mode

--- Code/Program.cs
+++ Code/Program.cs
@@ -15,10 +15,60 @@ namespace CSharpScript
 {
     class Program
     {
-        static void PrintUsage()
+        const string HelloWorldTemplate = @"
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel.Primitives;
+using System.Data;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace CSharpScript
+{
+    class Program
+    {
+        static void Main(string[] args)
         {
-            Console.WriteLine(
-@"
+            Console.WriteLine(""Hello World!"");
+        }
+
+        static List<string> ExecuteCommand(string filename, string arguments)
+        {
+            ProcessStartInfo startInfo = new ProcessStartInfo();
+            startInfo.CreateNoWindow = false;
+            startInfo.UseShellExecute = false;
+            startInfo.FileName = filename;
+            startInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            startInfo.Arguments = arguments;
+            startInfo.RedirectStandardOutput = true;
+
+            List<string> outputLines = new List<string>();
+            try
+            {
+                Process exeProcess = Process.Start(startInfo);
+                while (!exeProcess.StandardOutput.EndOfStream)
+                {
+                    outputLines.Add(exeProcess.StandardOutput.ReadLine());
+                }
+            }
+            catch (Exception e)
+            {
+                // Log error.
+                outputLines.Add(e.ToString());
+            }
+
+            return outputLines;
+        }
+    }
+}
+";
+
+        const string Usage = @"
 C#.exe allows you to execute cs files from the command line.
 
 Usage: C# [switches] <filename> [args...]
@@ -32,8 +82,15 @@ Usage: C# [switches] <filename> [args...]
 
 [args...]
     Arguments to pass to the script when executed.
-");
+";
+
+        static void PrintUsage()
+        {
+            Console.WriteLine(Usage);
         }
+
+        static bool Verbose = false;
+        static void VerboseWrite(string message) { if (Verbose) Console.WriteLine(message); }
 
         static void Main(string[] args)
         {
@@ -43,39 +100,48 @@ Usage: C# [switches] <filename> [args...]
                 PrintUsage();
                 return;
             }
-            
-            string filename = args.FirstOrDefault(arg => !arg.StartsWith("-"));
-            if (filename == null)
+
+            Verbose = args.Any(arg => arg == "-v" || arg == "-verbose");
+
+            string codeFilename = args.FirstOrDefault(arg => !arg.StartsWith("-"));
+            bool outputTemplate = args.Any(arg => arg == "-t" || arg == "-template");
+            bool force = args.Any(arg => arg == "-f" || arg == "-force");
+
+            if (codeFilename == null)
             {
                 Console.WriteLine($"Missing argument for C# file to execute.");
                 PrintUsage();
                 return;
             }
             
-            bool OutputTemplate = args.Any(arg => arg == "-t" || arg == "-template");
-            if (OutputTemplate)
+            if (outputTemplate)
             {
-                WriteTemplateFile(filename);
+                VerboseWrite($"Writing template file \"{codeFilename}\".");
+                WriteTemplateFile(codeFilename, force);
                 return;
             }
-            else if (!File.Exists(filename))
+            else if (!File.Exists(codeFilename))
             {
                 Console.WriteLine($"Can't find file  \"{args[0]}\" in current directory: {System.IO.Directory.GetCurrentDirectory()}");
                 PrintUsage();
                 return;
             }
 
-            string codeFilename = args[0];
-            string exeFilename = Compile(codeFilename);
-            
-            Execute(exeFilename, args.Skip(1).ToArray());
+
+            string exeFilename;
+            VerboseWrite($"Compiling file \"{codeFilename}\".");
+            if (Compile(codeFilename, out exeFilename, force))
+            {
+                VerboseWrite($"Compilation successful. Executing \"{exeFilename}\".");
+                Execute(exeFilename, args.Skip(1).ToArray());
+            }
         }
 
-        private static string Compile(string codeFilename)
+        private static bool Compile(string codeFilename, out string exeFilename, bool force)
         {
-            string tempFolderName = ".tmp";
+            string tempFolderName = ".cstmp"; // TODO: Should this be beside the target code file?
             
-            string exeFilename = Path.Combine(Directory.GetCurrentDirectory(), tempFolderName, System.IO.Path.GetFileNameWithoutExtension(codeFilename) + ".exe");
+            exeFilename = Path.Combine(Directory.GetCurrentDirectory(), tempFolderName, System.IO.Path.GetFileNameWithoutExtension(codeFilename) + ".exe");
 
             if (!System.IO.Directory.Exists(tempFolderName))
             {
@@ -83,16 +149,22 @@ Usage: C# [switches] <filename> [args...]
             }
             else if (File.Exists(exeFilename))
             {
-                var lastCompilationTime = File.GetLastWriteTimeUtc(exeFilename);
-                var lastCodeChangeTime = File.GetLastWriteTimeUtc(codeFilename);
-                TimeSpan timeDifference = (lastCompilationTime - lastCodeChangeTime).Duration();
-                bool alreadyCompiled = timeDifference < TimeSpan.FromSeconds(2);
-                if (alreadyCompiled)
+                if (force)
                 {
-                    return exeFilename;
+                    File.Delete(exeFilename);
+                }
+                else
+                {
+                    var lastCompilationTime = File.GetLastWriteTimeUtc(exeFilename);
+                    var lastCodeChangeTime = File.GetLastWriteTimeUtc(codeFilename);
+                    TimeSpan timeDifference = (lastCompilationTime - lastCodeChangeTime).Duration();
+                    bool alreadyCompiled = timeDifference < TimeSpan.FromSeconds(2);
+                    if (alreadyCompiled)
+                    {
+                        return true;
+                    }
                 }
             }
-
         
             string sourceCode = File.ReadAllText(codeFilename);
             var parsedSyntaxTree = SyntaxFactory.ParseSyntaxTree(sourceCode);
@@ -100,31 +172,38 @@ Usage: C# [switches] <filename> [args...]
             IEnumerable<string> DefaultNamespaces = new[]
             {
                 "System",
-                "System.IO",
-                "System.Net",
-                "System.Linq",
-                "System.Text",
-                "System.Text.RegularExpressions",
-                "System.Collections.Generic"
+                //"System.IO",
+                //"System.Net",
+                //"System.Linq",
+                //"System.Text",
+                //"System.Text.RegularExpressions",
+                //"System.Collections.Generic",
+                //"System.ComponentModel"
             };
 
-            string RuntimeDirectory = RuntimeEnvironment.GetRuntimeDirectory();
+            string runtimeDirectory = RuntimeEnvironment.GetRuntimeDirectory();
 
+            //https://stackoverflow.com/questions/58453115/roslyn-c-sharp-csharpcompilation-compiling-dynamic
             string[] DotNetLibraries = new[]
             {
-                RuntimeDirectory + "mscorlib.dll",
-                RuntimeDirectory + "System.dll",
-                RuntimeDirectory + "System.Core.dll",
+                runtimeDirectory + "mscorlib.dll",
+                runtimeDirectory + "System.dll",
+                runtimeDirectory + "System.Collections.dll",
+                runtimeDirectory + "System.Console.dll",
+                runtimeDirectory + "System.Core.dll",
+                runtimeDirectory + "System.Data.dll",
+                runtimeDirectory + "System.IO.dll",
+                runtimeDirectory + "System.Linq.dll",
+                runtimeDirectory + "System.Linq.Parallel.dll",
+                runtimeDirectory + "System.Runtime.dll",
+                runtimeDirectory + "System.Runtime.Extensions.dll", //System.Runtime.Extensions
+
                 typeof(object).GetTypeInfo().Assembly.Location,
-                RuntimeDirectory + "System.Runtime.dll",
-                RuntimeDirectory + "System.Diagnostics.Debug.dll",
-                RuntimeDirectory + "System.IO.dll",
-                RuntimeDirectory + "System.Linq.dll",
-                RuntimeDirectory + "System.Linq.Parallel.dll",
-                RuntimeDirectory + "System.Console.dll"
+                typeof(System.Diagnostics.Process).GetTypeInfo().Assembly.Location,
+                typeof(System.ComponentModel.Component).GetTypeInfo().Assembly.Location
             };
 
-            CSharpCompilationOptions DefaultCompilationOptions =
+            CSharpCompilationOptions defaultCompilationOptions =
                 new CSharpCompilationOptions(OutputKind.ConsoleApplication)
                 .WithOverflowChecks(true)
                 .WithOptimizationLevel(OptimizationLevel.Release)
@@ -133,7 +212,7 @@ Usage: C# [switches] <filename> [args...]
             string assemblyName = "CSharpScriptAssembly";
             var compilation = CSharpCompilation.Create(assemblyName)
                 .AddReferences(DotNetLibraries.Select(x => MetadataReference.CreateFromFile(x)))
-                .WithOptions(DefaultCompilationOptions)
+                .WithOptions(defaultCompilationOptions)
                 .AddSyntaxTrees(parsedSyntaxTree);
 
             var result = compilation.Emit(exeFilename);
@@ -144,9 +223,10 @@ Usage: C# [switches] <filename> [args...]
                 {
                     Console.WriteLine(diagnostic);
                 }
+                return false;
             }
 
-            return exeFilename;
+            return true;
         }
 
         private static void Execute(string filename, string[] args)
@@ -163,31 +243,8 @@ Usage: C# [switches] <filename> [args...]
             method.Invoke(null, new[] { args });
         }
 
-        private static void WriteTemplateFile(string filename)
+        private static void WriteTemplateFile(string filename, bool force)
         {
-            const string HelloWorldTemplate =
-@"
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.IO;
-using System.Linq;
-using System.Reflection;
-using System.Runtime.InteropServices;
-using System.Text;
-
-namespace CSharpScript
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine(""Hello World!"");
-        }
-    }
-}
-";
             if (string.IsNullOrWhiteSpace(Path.GetExtension(filename)))
             {
                 filename += ".cs";
@@ -195,8 +252,15 @@ namespace CSharpScript
 
             if (File.Exists(filename))
             {
-                Console.WriteLine($"Can't output template file: {filename} already exists.");
-                return;
+                if (force)
+                {
+                    File.Delete(filename);
+                }
+                else
+                {
+                    Console.WriteLine($"Can't output template file: {filename} already exists.");
+                    return;
+                }
             }
 
             File.WriteAllText(filename, HelloWorldTemplate);
